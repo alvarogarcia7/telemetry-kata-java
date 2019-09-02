908337f810cbc2a7724418527bac9ffd41b96b04
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-09-02 20:20:02 +0200

    Refactor: encapsulate retry logic into another class

--- src/main/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControls.java
+++ src/main/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControls.java
@@ -3,47 +3,81 @@ package tddmicroexercises.telemetrysystem;
 public class TelemetryDiagnosticControls
 {
     private final String DiagnosticChannelConnectionString = "*111#";
-    
+
     private final TelemetryClient telemetryClient;
     private String diagnosticInfo = "";
 
         public TelemetryDiagnosticControls()
         {
-            telemetryClient = new TelemetryClient();
+	        this(new TelemetryClient());
         }
 
         public TelemetryDiagnosticControls(TelemetryClient telemetryClient)
         {
-            this.telemetryClient = telemetryClient;
+	        this.telemetryClient = new RetriesTelemetryClient(telemetryClient);
         }
 
         public String getDiagnosticInfo(){
             return diagnosticInfo;
         }
-        
+
         public void setDiagnosticInfo(String diagnosticInfo){
             this.diagnosticInfo = diagnosticInfo;
         }
- 
+
         public void checkTransmission() throws Exception
         {
             diagnosticInfo = "";
 
             telemetryClient.disconnect();
-    
-            int retryLeft = 3;
-            while (!telemetryClient.getOnlineStatus() && retryLeft > 0)
-            {
-                telemetryClient.connect(DiagnosticChannelConnectionString);
-                retryLeft -= 1;
-            }
-             
-            if(!telemetryClient.getOnlineStatus())
-            {
-                throw new Exception("Unable to connect.");
-            }
-    
+
+            telemetryClient.connect(DiagnosticChannelConnectionString);
+
             telemetryClient.send(TelemetryClient.DIAGNOSTIC_MESSAGE);
             diagnosticInfo = telemetryClient.receive();
+    }
+
+    private class RetriesTelemetryClient extends TelemetryClient {
+
+	    private final TelemetryClient telemetryClient;
+	    private int retryLeft = 3;
+
+	    public RetriesTelemetryClient(TelemetryClient telemetryClient) {
+		    this.telemetryClient = telemetryClient;
+	    }
+
+	    @Override
+	    public void connect(String diagnosticChannelConnectionString) throws RuntimeException {
+            while (!telemetryClient.getOnlineStatus() && retryLeft > 0)
+            {
+                telemetryClient.connect(diagnosticChannelConnectionString);
+                retryLeft -= 1;
+            }
+
+            if(!telemetryClient.getOnlineStatus())
+            {
+                throw new RuntimeException("Unable to connect.");
+            }
+        }
+
+	    @Override
+	    public String receive() {
+		    return telemetryClient.receive();
+	    }
+
+	    @Override
+	    public boolean getOnlineStatus() {
+		    return telemetryClient.getOnlineStatus();
+	    }
+
+	    @Override
+	    public void disconnect() {
+		    telemetryClient.disconnect();
+	    }
+
+	    @Override
+	    public void send(String message) {
+		    telemetryClient.send(message);
+	    }
     }
 }
