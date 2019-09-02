37b3aaa92ecad0b394de05a3d64cbf2ababad6a4
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-09-02 20:08:32 +0200

    Refactor: simplify expression

--- src/main/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControls.java
+++ src/main/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControls.java
@@ -32,13 +32,13 @@ public class TelemetryDiagnosticControls
             telemetryClient.disconnect();
     
             int retryLeft = 3;
-            while (telemetryClient.getOnlineStatus() == false && retryLeft > 0)
+            while (!telemetryClient.getOnlineStatus() && retryLeft > 0)
             {
                 telemetryClient.connect(DiagnosticChannelConnectionString);
                 retryLeft -= 1;
             }
              
-            if(telemetryClient.getOnlineStatus() == false)
+            if(!telemetryClient.getOnlineStatus())
             {
                 throw new Exception("Unable to connect.");
             }
